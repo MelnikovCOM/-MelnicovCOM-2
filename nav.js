5f66cb9e9486010def1c7c994d07d5ca6807c739
Aleksei <melnik_89_1989@mail.ru>
2022-03-31 18:01:54 +0300

    add nav.css

--- nav.js
+++
@@ -1,3 +0,0 @@
-let button = document.getElementById('bur');
-let navigation = document.getElementById('popu');
-button.addEventListener('click', function (){navigation.classList.toggle('display-block');});
